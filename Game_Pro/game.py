96f0428d2a4f012b8bcb92d6f20e555ce15bc7f7
hengheak <heng.heak@student.passerellesnumeriques.org>
2021-12-27 11:09:10 +0700

    Move right with sound

--- Game_Pro/game.py
+++ Game_Pro/game.py
@@ -1,7 +1,8 @@
 #  IMPORTS------------------------------------------------------
+from os import cpu_count
 import tkinter as tk
 import winsound
-from typing import Generator
+# from typing import Generator
 from tkinter import messagebox
 #  MAIN Code-----------------------------------------------------
 root = tk.Tk()
@@ -37,18 +38,19 @@ myBackground=tk.PhotoImage(file='backg.png')
     #
     #Show when user Win!!!!!!----------------------------------
 myWiner=tk.PhotoImage(file='youwin.png')
+
     #Show when user Loose!!!-----------------------------------
 myLoser=tk.PhotoImage(file='gameover.png')
 
 
-#Note==========================================================
+#Note----------------------------------------------------
 empty=0
 wall=1
 player=2
 coin=3
 monster=4
 goal=5
-#==============================================================
+#--------------------------------------------------------
 
 #winsound
 winsound.PlaySound("startgame.wav", winsound.SND_FILENAME|winsound.SND_ASYNC)
@@ -99,25 +101,42 @@ def arrayToDrawing():
                     canvas.create_image(x1+26,y1+27,image=myCoin)
             else:
                 canvas.create_image(x1+23,y1+27,image=myImage)
+
     canvas.create_text(350,30,fill="yellow",font="Times 16  bold",text="Score: "+str(Coins))
     canvas.create_text(500,30,fill="yellow",font="Times 16  bold",text="Time Left: "+str(Moving))
     canvas.create_text(300,75,fill="yellow",font="Times 35  bold",text="Eat Coin!"+str(NameOfGame))
     return None
-####################heng
-def moveRight(event):
-    global grid
-    stoped=False
-    oldValue=0
-    for row in range(len(grid)):
-        for coml in range(len(grid[row])-1):
-            if grid[row][coml]==2 and not stoped and grid[row][coml+1]!=1:
-                grid[row][coml]=0
-                grid[row][coml+1]=2
-                stoped=True
-                
-    print(grid)
-    arrayToDrawing()
+arrayToDrawing()
+
+#MoveRight------------------------
+def PositionOfPlayer(grid) :
+    for column in range(len(grid)):
+        for row in range(len(grid[column])):
+            if grid[column][row]== 2:
+                position=[column,row]
+    return position
+
+def MoveRight(event):
+    global grid, Coins,Moving
+    Moving=Moving-1
+    position = PositionOfPlayer(grid)
+    playerrow = position[0]
+    playercolumn = position[1]
+    oldValue = 0
+    if grid[playerrow][playercolumn +1]!=1:
+        grid[playerrow][playercolumn ]=0
+        oldValue = grid[playerrow][playercolumn+1]
+        grid[playerrow][playercolumn +1]=2
     
+    if oldValue==coin:
+        winsound .PlaySound('coin.wav', winsound.SND_FILENAME)
+        Coins=Coins+100
+    arrayToDrawing() 
+               
+
+
+
+#Move Left--------------------------------------
 def moveLeft(event):
     global grid
     canvas.delete("all")
@@ -131,6 +150,8 @@ def moveLeft(event):
            
     print(grid)
     arrayToDrawing()
+
+#Move Down----------------------------------------------
 def moveDown(event):
     global grid
     stoped=False
@@ -143,6 +164,8 @@ def moveDown(event):
     print(grid)
     arrayToDrawing()
 
+
+#Move Up-----------------------------------------------
 def moveUp(event):
     global grid
     stoped=False
@@ -154,10 +177,11 @@ def moveUp(event):
                 stoped=True
     print(grid)
     arrayToDrawing()
-root.bind("<Right>",moveRight)    
-root.bind("<Left>",moveLeft)    
+
+root.bind("<Right>",MoveRight)#Right click    
+root.bind("<Left>",moveLeft)#Left click    
 root.bind("<Up>", moveUp) #Up CLICK
-root.bind("<Down>", moveDown)
+root.bind("<Down>", moveDown)#Down clik
 canvas.pack(expand=True, fill="both")
-arrayToDrawing()
+
 root.mainloop()
